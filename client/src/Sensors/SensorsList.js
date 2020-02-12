a4b5e290279374778b51d3446c8e733f1a65e52a
Oleh Komarnytskyi <komolebo@gmail.com>
2020-02-12 18:56:44 -0500

    New sensor popup

--- client/src/Sensors/SensorsList.js
+++ client/src/Sensors/SensorsList.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './SensorsList.css';
 import SensorItem  from './SensorItem';
 import { Sensor } from './SensorItem';
-
+import { popupAddNewSensor, SensorPopup } from "./SensorPopup";
 
 const displayProperties = [
     'ID',
@@ -29,26 +29,45 @@ class SensorsList extends Component {
         super();
     }
 
+    addSensorItem(sn, name="Generic", status=true, description="Generic device, please append new data here") {
+        this.state.sensors.push(new Sensor(sn, name, status, description));
+        this.setState( { sensors: this.state.sensors } );
+    }
+
+    /* Debug part only, will be replaced by socket.io functionality */
+    onAddItem() {
+        const sn = "0xFFAB8271940302";
+        console.log(this);
+        popupAddNewSensor(sn, this.addSensorItem.bind(this));
+
+    }
+
     onRemoveItem(sensorId) {
         this.setState({sensors: this.state.sensors.filter(item => (item.id !== sensorId))});
     }
 
     render() {
         return (
-            <table id='sensors'>
-            <thead key="thead">
-                <tr>
-                    { displayProperties.map( (sensorProperty) =>
-                        <th> { sensorProperty } </th>
-                    )}
-                </tr>
-            </thead>
-            <tbody key="tbody">
-                { this.state.sensors.map( ( sensor ) =>
-                    <SensorItem data={sensor} onRemove={this.onRemoveItem.bind(this)} />
-                )}
-            </tbody>
-        </table>
+            <div>
+                <table id='sensors'>
+                    <thead key="thead">
+                        <tr>
+                            { displayProperties.map( (sensorProperty) =>
+                                <th> { sensorProperty } </th>
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody key="tbody">
+                        { this.state.sensors.map( ( sensor ) =>
+                            <SensorItem data={sensor} onRemove={this.onRemoveItem.bind(this)} />
+                        )}
+                    </tbody>
+                </table>
+                <SensorPopup />
+
+                {/* Debug part only, will be replaced by socket.io functionality */}
+                <button className="sensor-btn-delete" onClick={this.onAddItem.bind(this)}>Simulate adding new device</button>
+            </div>
         )
     }
 }
