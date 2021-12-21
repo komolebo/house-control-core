3844a336a9021dfefa510b56f89c91232c0556c1
Oleh <komolebo@gmail.com>
2021-12-21 13:39:32 +0000

    Fix websocket path

--- app/routing.py
+++ app/routing.py
@@ -3,6 +3,6 @@ from django.urls.conf import re_path
 from house_control.consumers import EventConsumer
 
 websocket_urlpatterns = [
-    re_path('ws/sensor', EventConsumer)
+    re_path('ws/sensors', EventConsumer)
     # url(r"^ws/sensors/$", EventConsumer),
 ]
